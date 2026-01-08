64bc51ab61ba9ea11c418ad57660c09277fad964
David Ko <davidsj.ko@gmail.com>
2026-01-08 04:56:36 -0500

    Add xcode proj

---
+++ internal/focusdetect/focusdetector/focusdetector/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  focusdetector
+//
+//  Created by David Yoon on 1/8/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
